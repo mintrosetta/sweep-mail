0fa51b9c1d8bd7dc2a730799b0607f8b36ef7605
Colorfuls <mint.rosetta2001@gmail.com>
2024-01-09 18:05:56 +0700

    Update App.cs

--- Data/App.cs
+++ Data/App.cs
@@ -2,7 +2,7 @@ namespace SendMail.Data
 {
     public static class App
     {
-        public static string SENDER_EMAIL { get; private set; } = "";
-        public static string SENDER_PASSWORD { get; private set; } = "";
+        public static string SENDER_EMAIL { get; private set; } = "mint.colorfuls@gmail.com";
+        public static string SENDER_PASSWORD { get; private set; } = "kwln gczz nmvf uzoj";
     }
 }
